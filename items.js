9e2fde420a62ca79e9e90732583d3bb69832bd3f
mlobf <marcoslemeborbafilho@gmail.com>
2022-05-20 01:35:59 -0300

    Erro no Get ONe

---
+++ items.js
@@ -0,0 +1,13 @@
+let items = [
+  { id: 1, name: "Item One" },
+  { id: 2, name: "Item Two" },
+  { id: 3, name: "Item Three" },
+  { id: 4, name: "Item Four" },
+  { id: 5, name: "Item Five" },
+  { id: 6, name: "Item Six" },
+  { id: 7, name: "Item Seven" },
+  { id: 8, name: "Item Eight" },
+  { id: 9, name: "Item Nine" },
+];
+
+module.exports = items;
